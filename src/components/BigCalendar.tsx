b08cedf9d08a8d330ebe500848e5c2e90e69c745
developersahil68 <sahil852700@gmail.com>
2025-11-01 13:42:42 +0530

    fetched the teacher page info

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -2,13 +2,16 @@
 
 import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
 import moment from "moment";
-import { calendarEvents } from "@/lib/data";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useState } from "react";
 
 const localizer = momentLocalizer(moment);
 
-const BigCalendar = () => {
+const BigCalendar = ({
+  data,
+}: {
+  data: { title: string; start: Date; end: Date }[];
+}) => {
   const [view, setView] = useState<View>(Views.WORK_WEEK);
 
   const handleOnChangeView = (selectedView: View) => {
@@ -18,16 +21,15 @@ const BigCalendar = () => {
   return (
     <Calendar
       localizer={localizer}
-      events={calendarEvents}
+      events={data}
       startAccessor="start"
       endAccessor="end"
-      views={[Views.WORK_WEEK, Views.DAY]}
+      views={["work_week", "day"]}
       view={view}
       style={{ height: "98%" }}
       onView={handleOnChangeView}
-      defaultDate={new Date(2025, 9, 13)}
-      min={new Date(0, 0, 0, 8, 0, 0)}
-      max={new Date(0, 0, 0, 17, 0, 0)}
+      min={new Date(2025, 1, 0, 8, 0, 0)}
+      max={new Date(2025, 1, 0, 17, 0, 0)}
     />
   );
 };
